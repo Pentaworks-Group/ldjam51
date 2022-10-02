e7132020e72019dd4fa2a7e3f623b3008781f37b
XpArKeR <xparker@gmx.net>
2022-10-02 14:37:34 +0200

    Changed Tile.Material property

--- Code/ldjam51/Assets/Scripts/Model/Tile.cs
+++ Code/ldjam51/Assets/Scripts/Model/Tile.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using Newtonsoft.Json;
+
 using UnityEngine;
 
 namespace Assets.Scripts.Game
@@ -11,6 +13,29 @@ namespace Assets.Scripts.Game
         public Boolean IsFinish { get; set; }
         public Boolean IsDeadly { get; set; }
         public Tile ExtraTemplate { get; set; }
-        public Material Material { get; set; }
+        public String MaterialReference { get; set; }
+
+        private Material material;
+        [JsonIgnore]
+        public Material Material
+        {
+            get
+            {
+                if ((this.material == default) && (!String.IsNullOrEmpty(MaterialReference)))
+                {
+                    this.material = GameFrame.Base.Resources.Manager.Materials.Get(MaterialReference);
+                }
+
+                return material;
+            }
+            set
+            {
+                if (this.material != value)
+                {
+                    this.material = value;
+                    this.MaterialReference = value?.name;
+                }
+            }
+        }
     }
 }
