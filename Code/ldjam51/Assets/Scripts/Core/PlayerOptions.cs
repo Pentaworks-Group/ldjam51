e816c0edf46c43461d059a289c24d066b5093fb3
XpArKeR <xparker@gmx.net>
2022-10-03 09:50:44 +0200

    Fixed activeField on first load.

--- Code/ldjam51/Assets/Scripts/Core/PlayerOptions.cs
+++ Code/ldjam51/Assets/Scripts/Core/PlayerOptions.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Assets.Scripts.Core
 {
     public class PlayerOptions : GameFrame.Core.PlayerOptions
     {
         public String MobileInterface { get; set; }
-
     }
 }
